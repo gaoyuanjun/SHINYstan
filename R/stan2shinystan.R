e766f4850c3adccebb1cec41dc8d7a6c0ed810ba
jgabry <jgabry@dyn-160-39-143-190.dyn.columbia.edu>
2014-11-13 11:39:09 -0500

    fixed bug causing error for thin > 1

--- R/stan2shinystan.R
+++ R/stan2shinystan.R
@@ -6,20 +6,22 @@ stan2shinystan <- function(stanfit, notes) {
     stop(paste(name, "is not a stanfit object."))
   }
 
+  samps_all <- rstan::extract(stanfit, permuted = FALSE, inc_warmup = TRUE)
+
   slots <- list()
   slots$Class <- "shinystan"
   slots$model_name <- stanfit@model_name
   slots$param_names <- stanfit@sim$fnames_oi
   slots$param_dims <- stanfit@par_dims
   slots$param_groups <- names(stanfit@par_dims)
-  slots$samps_all <- rstan::extract(stanfit, permuted = FALSE, inc_warmup = TRUE)
-#   slots$samps_post_warmup <- rstan::extract(stanfit, permuted = FALSE, inc_warmup = FALSE)
+  slots$samps_all <- samps_all
   slots$summary <- rstan::summary(stanfit)$summary
   slots$sampler_params <- rstan::get_sampler_params(stanfit)
   slots$nChains <- ncol(stanfit)
-  slots$nIter <- stanfit@sim$iter
-  slots$nWarmup <- stanfit@sim$warmup
+  slots$nIter <- nrow(samps_all) # total number of iterations (after thinning)
+  slots$nWarmup <- floor(stanfit@sim$warmup / stanfit@sim$thin)
   if (!missing(notes)) slots$user_model_info <- notes
 
   do.call("new", slots)
 }
+
--- inst/SHINYstan/server_files/extract_shiny_stan_object.R
+++ inst/SHINYstan/server_files/extract_shiny_stan_object.R
@@ -1,10 +1,10 @@
 # Extract the content of the shiny_stan_object slots
 object <- shiny_stan_object
 samps_all <- object@samps_all
-# samps_post_warmup <- object@samps_post_warmup
 sampler_params <- object@sampler_params
 nIter <- object@nIter
 warmup_val <- object@nWarmup
 samps_post_warmup <- samps_all[(warmup_val + 1):nIter,,]
+# samps_post_warmup <- object@samps_post_warmup
 fit_summary <- object@summary
 param_names <- object@param_names
